841fb80ee02b9525f841b234ba5c4216154d6639
javacrim <javacrim@d3d56f30-b0ff-4ed1-be4c-92d0b32de53b>
2012-04-03 19:34:04 +0000

    Ovar tagger, SQLite

--- littre/web-app/WEB-INF/java/fr/crim/littre/xml/DemoTagger.java
+++ littre/web-app/WEB-INF/java/fr/crim/littre/xml/DemoTagger.java
@@ -10,12 +10,13 @@ public class DemoTagger extends Tagger {
 	public Node tag(Node n) {
 		Node ret;
 		String xml=dom2string(n);
+		System.out.println(xml);
 		try {
 			ret=string2dom(xml);
 		}
 		catch (Exception e) {
-			System.err.println(this.getClass().getName()+".tag(), XML error: "+xml);
-			return n;
+			System.err.println("\t"+this.getClass().getName()+".tag(), XML error in  "+this.file+"#"+getId(n)+"\n"+xml);
+ 			return n;
 		}
 		return ret;
 	}
@@ -25,15 +26,15 @@ public class DemoTagger extends Tagger {
 	 */
 	public static void main(String[] args) throws Exception {
 		File xmlDir = new File("xml");
-		String xpath = "//auote";
+		String xpath = "//quote";
 		if (args.length > 1)  xpath=args[0];
 		if (args.length > 2) xmlDir = new File(args[1]);
 		// parcourir la liste de fichiers
 		Tagger tagger=new DemoTagger();
-		for (File file : xmlDir.listFiles(new GlobFilter("k.xml"))) {
+		for (File file : xmlDir.listFiles(new GlobFilter("test.tei"))) {
 			Document doc=tagger.parse(file, xpath);
 			// voir ici ce qu'on fait avec le résultat, prudence avant de remplacer
-			System.out.println(dom2string(doc));
+			// System.out.println(dom2string(doc));
 		}
 	}
 
